d679fe88b47bdf69447d2c85754779aa72f303ec
林家齊 <a437777@gmail.com>
2025-08-07 15:23:56 +0800

    更新上傳

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/Week2Vue/', // 對應 repo 名稱
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
